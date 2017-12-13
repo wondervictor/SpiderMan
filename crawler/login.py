ba0cd03f6d63a1ed1c5cef6322f70cf07b6e1f30
hustyyq <hustyyq@gmail.com>
2017-12-13 11:11:45 +0800

    Update Login

--- crawler/login.py
+++ crawler/login.py
@@ -1,55 +1,129 @@
 # -*- coding: utf-8 -*-
-
 import requests
-import ConfigParser
+import cookielib
+import re
+import time
+import os.path
+try:
+    from PIL import Image
+except:
+    pass
 
-def create_session():
-    cf = ConfigParser.ConfigParser()
-    cf.read('config.ini')
-    cookies = cf.items('cookies')
-    cookies = dict(cookies)
-    from pprint import pprint
-    pprint(cookies)
-    email = cf.get('info', 'email')
-    password = cf.get('info', 'password')
 
-    session = requests.session()
-    login_data = {'email': email, 'password': password}
-    header = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36',
-        'Host': 'www.zhihu.com',
-        'Referer': 'http://www.zhihu.com/'
-    }
-    r = session.post('http://www.zhihu.com/login/email', data=login_data, headers=header)
-    if r.json()['r'] == 1:
-        print 'Login Failed, reason is:',
-        for m in r.json()['data']:
-            print r.json()['data'][m]
-        print 'So we use cookies to login in...'
-        has_cookies = False
-        for key in cookies:
-            if key != '__name__' and cookies[key] != '':
-                has_cookies = True
-                break
-        if has_cookies is False:
-            raise ValueError('请填写config.ini文件中的cookies项.')
+# 构造 Request headers
+headers = {
+    "Host": "www.zhihu.com",
+    "Referer": "https://www.zhihu.com/",
+    "User-agent" : 'Mozilla/5.0'
+}
+head = {'user-agent':'Mozilla/5.0'}
+
+# 使用登录cookie信息
+session = requests.session()
+session.cookies = cookielib.LWPCookieJar(filename='zhihucookie')
+try:
+    session.cookies.load(ignore_discard=True)
+except:
+    print('cookie 文件未能加载')
+
+
+def get_xsrf():
+    '''_xsrf 是一个动态变化的参数'''
+    index_url = 'https://www.zhihu.com/#signin'
+    # 获取登录时需要用到的_xsrf
+    index_page = requests.get("https://www.zhihu.com/#signin",headers=head)
+    index_page.raise_for_status()
+    index_page.encoding = index_page.apparent_encoding
+    html = index_page.text
+    # print(html[:10000])
+    pattern = r'name="_xsrf" value="(.*?)"'
+    # 这里的_xsrf 返回的是一个list
+    _xsrf = re.findall(pattern, html)
+    return _xsrf[0]
+
+
+# 获取验证码
+def get_captcha():
+    t = str(int(time.time() * 1000))
+    captcha_url = 'https://www.zhihu.com/captcha.gif?r=' + t + "&type=login"
+    r = session.get(captcha_url, headers=headers)
+    with open('captcha.jpg', 'wb') as f:
+        f.write(r.content)
+        f.close()
+    # 用pillow 的 Image 显示验证码
+    # 如果没有安装 pillow 到源代码所在的目录去找到验证码然后手动输入
+    try:
+        im = Image.open('captcha.jpg')
+        im.show()
+        im.close()
+    except:
+        print(u'请到 %s 目录找到captcha.jpg 手动输入' % os.path.abspath('captcha.jpg'))
+    captcha = input("please input the captcha\n>")
+    return captcha
+
+
+def isLogin():
+    # 通过查看用户个人信息来判断是否已经登录
+    url = "https://www.zhihu.com/settings/profile"
+    login_code = session.get(url, headers=headers, allow_redirects=False).status_code
+    if login_code == 200:
+        return True
+    else:
+        return False
+
+
+def login(secret, account):
+    _xsrf = get_xsrf()
+    headers["X-Xsrftoken"] = _xsrf
+    headers["X-Requested-With"] = "XMLHttpRequest"
+    # 通过输入的用户名判断是否是手机号
+    if re.match(r"^1\d{10}$", account):
+        print("手机号登录 \n")
+        post_url = 'https://www.zhihu.com/login/phone_num'
+        postdata = {
+            '_xsrf': _xsrf,
+            'phone_num': account,
+            'password': secret
+        }
+    else:
+        if "@" in account:
+            print("邮箱登录 \n")
         else:
-            # r = requests.get('http://www.zhihu.com/login/email', cookies=cookies) # 实现验证码登陆
-            r = session.get('http://www.zhihu.com/login/email', cookies=cookies) # 实现验证码登陆
+            print("你的账号输入有问题，请重新登录")
+            return
+        post_url = 'https://www.zhihu.com/login/email'
+        postdata = {
+            '_xsrf': _xsrf,
+            'email': account,
+            'password': secret
+        }
+    # 不需要验证码直接登录成功
+    loginresponse = session.post(url=post_url, headers=headers, data=postdata)
+    loginresponse.encoding = loginresponse.apparent_encoding
+    print('loginresponse', loginresponse.status_code)
+    # print(loginresponse.json())
+    # # 验证码问题输入导致失败: 猜测这个问题是由于session中对于验证码的请求过期导致
+    # if loginresponse.json()['r'] == 1:
+    #     # 不输入验证码登录失败
+    #     # 使用需要输入验证码的方式登录
+    #     postdata["captcha"] = get_captcha()
+    #     login_page = session.post(post_url, data=postdata, headers=headers)
+    #     login_code = login_page.json()
+    #     print(login_code['msg'])
+    # # 保存 cookies 到文件，
+    # # 下次可以使用 cookie 直接登录，不需要输入账号和密码
+    session.cookies.save()
 
-    with open('login.html', 'w') as fp:
-        fp.write(r.content)
-
-    return session, cookies
+try:
+    input = raw_input
+except:
+    pass
 
 
 if __name__ == '__main__':
-    requests_session, requests_cookies = create_session()
-
-    # url = 'http://www.zhihu.com/login/email'
-    url = 'http://www.zhihu.com/topic/19552832'
-    # content = requests_session.get(url).content # 未登陆
-    # content = requests.get(url, cookies=requests_cookies).content # 已登陆
-    content = requests_session.get(url, cookies=requests_cookies).content # 已登陆
-    with open('url.html', 'w') as fp:
-        fp.write(content)
+    if isLogin():
+        print('您已经登录')
+    else:
+        account = input('请输入你的用户名\n>  ')
+        secret = input("请输入你的密码\n>  ")
+        login(secret, account)
