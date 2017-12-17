ee794705df09b79da5dc6789d044743331a3dbff
hustyyq <hustyyq@gmail.com>
2017-12-17 23:12:12 +0800

    get all answers

--- crawler/geturl.py
+++ crawler/geturl.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 import requests
 
-def GetHTML(keyword):
+def GetHTML(url,keyword):
     try:
         head = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'}
         keyword = {'q':keyword}
-        r = requests.get("https://www.zhihu.com/search?",params= keyword, headers = head)
-        #r = requests.get("https://www.zhihu.com",headers=head)
+        url = url.split('/answer')[0]
+        #print(url)
+        if len(keyword)> 0:
+            r = requests.get(url,params= keyword, headers = head)
+        else:
+            r = requests.get(url,headers=head)
         r.raise_for_status()
         r.encoding = r.apparent_encoding
         print(r.text)
@@ -14,7 +18,9 @@ def GetHTML(keyword):
         print("faile")
 
 def main():
-    keywords = "Vic Chan"
-    GetHTML(keywords)
+    keyword = "Vic Chan"
+    url = "https://www.zhihu.com/search?"
+    #url = "https://www.zhihu.com/question/35855905/answer/261582944"
+    GetHTML(url,keyword)
 
 main()
