70bc969d70c5f3c231a33a8ee31ed7e8d1b90b67
hustyyq <hustyyq@gmail.com>
2017-12-18 21:30:49 +0800

    next page

--- crawler/geturl.py
+++ crawler/geturl.py
@@ -1,24 +1,46 @@
 # -*- coding: utf-8 -*-
+import time
+
 import requests
-import re
+from selenium import webdriver
+
+#import re
+#import json
+
+headers = {
+    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
+    "Referer": "http://www.zhihu.com/",
+    'Host': 'www.zhihu.com',
+}
 
 def GetHTML(url,keyword):
     try:
-        head = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'}
+        #head = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'}
         keyword = {'q':keyword}
         url = url.split('/answer')[0]
-        #print(url)
-        if len(keyword)> 0:
-            r = requests.get(url,params= keyword, headers = head)
+        print(len(keyword))
+        if len(keyword)> 1:
+            r = requests.get(url,params= keyword, headers = headers)
         else:
-            r = requests.get(url,headers=head)
+            r = requests.get(url,headers=headers)
+
         r.raise_for_status()
         r.encoding = r.apparent_encoding
-        session_token = re.findall(r'session_token=([0-9,a-z]{32})', r.text)[0]
-        auto = re.findall(r'carCompose&quot;:&quot;(.*?)&quot', r.text)[0]
-        print(session_token)
+        #需要提供执行地址
+        driver = webdriver.Firefox(executable_path='')
+        cookie = driver.get_cookies()
+        driver.get(url)
+        for i in range(10):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(3)
+        html = driver.page_source
+        print (html)
+        # session_token = re.findall(r'session_token=([0-9,a-z]{32})', r.text)[0]
+        # auto = re.findall(r'carCompose&quot;:&quot;(.*?)&quot', r.text)[0]
+        # print(session_token)
     except:
-        print("faile")
+        print ("crawl faile")
+
 
 def main():
     #keyword = "Vic Chan"
