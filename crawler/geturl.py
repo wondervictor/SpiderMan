7565cf97c97bfd7d27d3e1fa8a53263944d7f60e
hustyyq <hustyyq@gmail.com>
2017-12-19 23:20:36 +0800

    null

--- crawler/geturl.py
+++ crawler/geturl.py
@@ -14,7 +14,7 @@ headers = {
 }
 
 def GetHTML(url,keyword):
-    # try:
+    try:
         #head = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'}
         keyword = {'q':keyword}
         url = url.split('/answer')[0]
@@ -26,11 +26,12 @@ def GetHTML(url,keyword):
 
         r.raise_for_status()
         r.encoding = r.apparent_encoding
-        #需要提供执行地址
+        #需要提供运行机器上firefox执行地址
+        #host添加127.0.0.1 localhost
         driver = webdriver.Firefox()
         cookie = driver.get_cookies()
         driver.get(url)
-        for i in range(10):
+        for i in range(2):
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(3)
         html = driver.page_source
@@ -38,9 +39,9 @@ def GetHTML(url,keyword):
         # session_token = re.findall(r'session_token=([0-9,a-z]{32})', r.text)[0]
         # auto = re.findall(r'carCompose&quot;:&quot;(.*?)&quot', r.text)[0]
         # print(session_token)
-    # except:
-    #     print ("crawl faile")
-    #
+    except:
+        print ("crawl faile")
+
 
 def main():
     #keyword = "Vic Chan"
