5d60976ed38390320422a980a3f1064cc2ff82e9
hustyyq <hustyyq@gmail.com>
2017-12-28 17:42:17 +0800

    update

--- crawler/gethtml.py
+++ crawler/gethtml.py
@@ -6,7 +6,6 @@ sys.path.append('../')
 from common import common
 import requests
 from selenium import webdriver
-import phantomas
 
 headers = {
     "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
@@ -28,7 +27,7 @@ def get_html(url, keyword=""):
 
         r.raise_for_status()
         r.encoding = r.apparent_encoding
-        # 需要提供运行机器上PhantomJS/firefox执行地址
+        # 需要提供运行机器上PhantomJS执行地址
         # host添加127.0.0.1 localhost
         service_args = []
         # 关闭图片加载
@@ -104,10 +103,14 @@ def get_html(url, keyword=""):
         # driver.get("https://www.zhihu.com/settings/profile")
         # html = driver.page_source
         # print(html)
-        # # session_token = re.findall(r'session_token=([0-9,a-z]{32})', r.text)[0]
-        # auto = re.findall(r'carCompose&quot;:&quot;(.*?)&quot', r.text)[0]
-        # print(session_token)
+        driver.get(url)
+        time.sleep(0.5)
+        for i in range(2):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(3)
+        html = driver.page_source
         driver.quit()
+        return html
      except:
         print ("crawl failed")
 
@@ -116,7 +119,6 @@ def main():
     url = "https://www.zhihu.com/search?"
     # keyword = ""
     # url = "https://www.zhihu.com/question/65483475/answer/261582944"
-    get_html(url,keyword)
-
+    page = get_html(url,keyword)
 
 main()
