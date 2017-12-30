2402f050a99451356bf26bccf28a23930ac5b2be
wondervictor <victorchanchina@gmail.com>
2017-12-30 13:57:38 +0800

    add sh

---
+++ install_dependency.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/sh
+
+# Python Dependencies
+echo "Installing Python Dependencies"
+
+echo "Installing mmh3"
+sudo pip install mmh
+
+echo "Installing bitarray"
+sudo pip install birarray
+
+
+echo "Installed"
