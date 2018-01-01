7b500672d35fc2605bd32faa2ff2ee0227306977
wondervictor <victorchanchina@gmail.com>
2018-01-01 14:46:20 +0800

    answer file sentiment analysis

--- sentiment_analysis/inference.py
+++ sentiment_analysis/inference.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import codecs
 from snownlp import SnowNLP
 
 
@@ -15,6 +15,32 @@ def inference(sentence):
         return False, 0
 
 
-def inference_answers(filepath):
-    
-    pass
+def inference_answers(filepath, save_path):
+
+    """
+    Answer:
+    :param filepath:
+    :return:
+    """
+
+    with open(filepath, 'r') as f:
+        raw_content = f.read()
+
+    content = raw_content.split('\n\n')
+    title = content[0]
+    content = content[1:-2]
+
+    result = list()
+    result.append(title)
+
+    for con in content:
+        ans = con.split('\n')[2]
+        ans = ans.replace('[内容]:','')
+        prob = SnowNLP(ans).sentiments
+        con += '\n[正面情感概率]: %s\n' % prob
+        result.append(con)
+    content = '\n'.join(result)
+
+    with codecs.open(save_path, 'a+') as f:
+        f.write(content)
+
